50e21fac70daa544f1fc8f3e94769af0897ab8d4
ijromerob <ijromerob@gmail.com>
2025-04-11 06:57:09 -0600

    learning recursion in erlang

--- factorial_calculator.erl
+++ factorial_calculator.erl
@@ -7,14 +7,25 @@
 %   io:format("Hello, ~s!~n", [Name]).
 
 
--export([feel/1]).
+% -export([feel/1]).
 
-feel(T) when T < 0 -> io:format("Freezing ~n");
-feel(T) when 0 =< T, T =< 20  -> io:format("Cold ~n");
-feel(T) when 21 =< T, T =< 30  -> io:format("Warm ~n");
-feel(T) when 30 < T -> io:format("Hot ~n").
+% feel(T) when T < 0 -> io:format("Freezing ~n");
+% feel(T) when 0 =< T, T =< 20  -> io:format("Cold ~n");
+% feel(T) when 21 =< T, T =< 30  -> io:format("Warm ~n");
+% feel(T) when 30 < T -> io:format("Hot ~n").
+
+% Recursion of a countdown 
+
+% -export([countdown/1]).
+
+% countdown(0) -> io:format("blast off!~n");
+% countdown(N) when N > 0 ->
+%     io:format("~p~n",[N]),
+%     countdown(N-1).
 
 
 
+-export([sum/1]).
 
-
+sum(0) -> 0;
+sum(N) when N > 0 -> N + sum(N-1).
