ce9c14b2fc94a0952dc18cc5a0660dc0073c404c
ijromerob <ijromerob@gmail.com>
2025-04-12 10:32:47 -0600

    adding comments and recursion adjustments

--- factorial_calculator.erl
+++ factorial_calculator.erl
@@ -1,42 +1,49 @@
 %this will have the module for the factorial calculator
 -module(factorial_calculator).
 
-% normal factorial
-
-% -export([fact/1]).
-
-% fact(0)-> 1;
-% fact(N) when N > 0 -> N * fact(N - 1);
-% fact(N) when N < 0 -> io:format("This is not allowed ~n").
-
-% -export([start/0]).
-
-% start() ->
-%     receive
-%         hello ->
-%             io:format("Hello received ~n"),
-%             start();
-%         goodbye ->
-%             io:format("Goodbye received ! ~n"),
-%             start();
-%         _ ->
-%             io:format("Unknown message ~n")
-%     end.
-
+% The export shows the functions that can be accessed outside of the module
+% the number after the "/" is the number of arguments (arity)
 -export([start/0, loop/0]).
 
+% This function starts creating a new process
 start()->
+  % The new process will use the current module with function loop and zero arguments
   spawn(?MODULE,loop,[]).
 
+% This function creates a loop with recursion
 loop() ->
+  % the receiver checks for specific inputs
   receive
-    {say, Msg} ->
-      io:format("received: ~p~n",[Msg]),
+    % checks for the message structure
+    % factorial tag and sends the process that called the function
+    % positive numbers
+    {factorial, From, N} when N >= 0 ->
+      % the Result is sent back the process (shell)
+      Result = fact(N),
+      From ! {result, N, Result},
+      % calls itself again
       loop();
+
+    %checks for negative numbers
+    {factorial, _, N} when N< 0 ->
+      io:format("Impossible to compute, it is a negative number: ~p~n", [N]),
+      loop();
+
+    % stops the process
     stop ->
-      io:format("stopping ~n"),
+      io:format("stopping the process ~n"),
       ok;
+
+    % checks for any other format message sent
     _ ->
-      io:format("unhandlend ~n"),
+      io:format("This is embarrassing I don't understand what you said! :/ ~n"),
       loop()
   end.
+
+% this checks for the factorial cases
+% base case in recursion
+fact(0)-> 1;
+% recursive guard when N is positive using recursion
+fact(N) when N > 0 -> N * fact(N - 1);
+% guard whenever there is a negative number
+fact(N) when N < 0 -> io:format("This is not allowed ~n").
