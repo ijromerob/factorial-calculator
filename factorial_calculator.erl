8e68d5d18a7fd06ed9cf91241d1a23564e0814d8
ijromerob <ijromerob@gmail.com>
2025-04-11 21:36:42 -0600

    going through loop

--- factorial_calculator.erl
+++ factorial_calculator.erl
@@ -1,7 +1,7 @@
 %this will have the module for the factorial calculator
 -module(factorial_calculator).
 
-% normal factorial 
+% normal factorial
 
 % -export([fact/1]).
 
@@ -9,14 +9,34 @@
 % fact(N) when N > 0 -> N * fact(N - 1);
 % fact(N) when N < 0 -> io:format("This is not allowed ~n").
 
--export([start/0]).
+% -export([start/0]).
 
-start() ->
-    receive
-        hello ->
-            io:format("Hello received ~n");
-        goodbye ->
-            io:format("Goodbye received ! ~n");
-        _ ->
-            io:format("Unknown message ~n")
-    end.
+% start() ->
+%     receive
+%         hello ->
+%             io:format("Hello received ~n"),
+%             start();
+%         goodbye ->
+%             io:format("Goodbye received ! ~n"),
+%             start();
+%         _ ->
+%             io:format("Unknown message ~n")
+%     end.
+
+-export([start/0, loop/0]).
+
+start()->
+  spawn(?MODULE,loop,[]).
+
+loop() ->
+  receive
+    {say, Msg} ->
+      io:format("received: ~p~n",[Msg]),
+      loop();
+    stop ->
+      io:format("stopping ~n"),
+      ok;
+    _ ->
+      io:format("unhandlend ~n"),
+      loop()
+  end.
